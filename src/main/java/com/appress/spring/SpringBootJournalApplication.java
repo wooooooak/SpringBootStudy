ab11b1a223dae1c9a03b5cd5073fb818ef33fb14
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-06-14 01:40:55 +0900

    InitializingBean으로 샘플데이터 추가

--- src/main/java/com/appress/spring/SpringBootJournalApplication.java
+++ src/main/java/com/appress/spring/SpringBootJournalApplication.java
@@ -1,11 +1,26 @@
 package com.appress.spring;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.appress.spring.domain.Journal;
+import com.appress.spring.repository.JournalRepository;
 
 @SpringBootApplication
 public class SpringBootJournalApplication {
 
+	@Bean
+	InitializingBean saveData(JournalRepository repo) {
+		return () -> {
+			repo.save(new Journal("스프링 부트 입문","오늘부터 스프링 부트 공부하기 시작했다","01/01/2016"));
+            repo.save(new Journal("간단한 스프링 부트 프로젝트","스프링 부트 프로젝트를 처음 만들어 보았다","01/02/2016"));
+            repo.save(new Journal("스프링 부트 해부","스프링 부트를 자세히 살펴보았다","02/01/2016"));
+            repo.save(new Journal("스프링 부트 클라우드","클라우드 파운드리를 응용한 스프링 부트를 공부했다","03/01/2016"));
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJournalApplication.class, args);
 	}
